1b9ed76539a8e248b11465f2f4524b714444af71
ZafCode14 <zafarwp14@gmail.com>
2024-01-14 08:00:02 +0200

    3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -18,7 +18,7 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception:
+    except:
         return None
 
 
@@ -40,7 +40,7 @@ def do_deploy(archive_path):
         run('ln -s {}{}/ /data/web_static/current'.format(path, date_time))
         print("New version deployed!")
         return True
-    except Exception:
+    except:
         return False
 
 
